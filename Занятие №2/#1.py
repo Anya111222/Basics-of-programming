c727a4d0a23535bce3a9498208c05c8f07aa7475
Anya111222 <146580890+Anya111222@users.noreply.github.com>
2023-10-01 00:53:28 +0300

    Занятие №2

---
+++ Занятие №2/#1.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+x = 14.26
+y = -1.22
+z = 3.5*(10**-2)
+import math
+a = 2*math.cos(x-2/3)
+b = 1/2 + (math.sin(y)**2)
+c = z**2
+d = 3 - ((z**2)/5)
+e = 1
+f = e + (c/d)
+s = (a/b)*f
+print(s)
---
+++ Занятие №2/#3.py
@@ -0,0 +1,19 @@
+# -- coding: utf-8 --
+x = 3.74*(10**(-2))
+y = -0.825
+z = 0.16*(10**2)
+
+import math
+
+a = (1 + (math.sin(x+y)**2))
+b = 2*y  
+k = ( 1 + ( (x**2) * (y**2)) )
+o = (b/(k))
+c = (abs(x - (o)))
+p = (a / (c))
+l = (x ** (abs(y)))
+d = (p * l)
+j = (math.cos( math.atan( (1/(z))) )**2)
+
+s = d + j
+print(s)
