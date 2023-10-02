28a1e6b3cd826eacc0b5b368816484777f74389d
Anya111222 <146580890+Anya111222@users.noreply.github.com>
2023-10-02 21:30:28 +0300

    Занятие №4

--- Занятие №4/#10.py
+++ Занятие №4/#10.py
@@ -1,9 +1,7 @@
 # -- coding: utf-8 --
 n=int(input())
 k=int(input())
-def F(n):
-   if n==1 or n==2:
-       return 1
-   else:
-       return F(n-2)+F(n-1)
-print((F(n+2)-1)[k-1:])
+nn=[0, 1]
+for i in range(2, n+1):
+    nn.append(nn[i-1] + nn[i-2])
+print(sum(nn[k-1:]))
--- Занятие №4/#9.py
+++ Занятие №4/#9.py
@@ -1,8 +1,6 @@
 # -- coding: utf-8 --
 n=int(input())
-def F(n):
-   if n==1 or n==2:
-       return 1
-   else:
-       return F(n-2)+F(n-1)
-print(F(n+2)-1)
+nn=[0, 1]
+for i in range(2, n+1):
+    nn.append(nn[i-1] + nn[i-2])
+print(sum(nn))
