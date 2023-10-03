e30205bd2b438853fe18baf2af86e8750ade1140
Anya111222 <146580890+Anya111222@users.noreply.github.com>
2023-10-03 17:56:26 +0300

    #7.py

--- Занятие №3/#7.py
+++ Занятие №3/#7.py
@@ -1,9 +1,9 @@
 # — coding: utf-8 — 
 def main(): 
- N = int(input()) 
+ N=int(input()) 
  if (N%4==0 and N%100!=0) or N%300==0: 
- return 'Да' 
+     return 'Да' 
  else: 
- return 'Нет' 
+     return 'Нет' 
  
 print(main())
