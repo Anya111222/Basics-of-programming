d0e9ad16b4d53f6c6131cd87840ce981275e55fb
Anya111222 <146580890+Anya111222@users.noreply.github.com>
2023-10-31 00:43:42 +0300

    Занятие №4

--- Занятие №4/#3.py
+++ Занятие №4/#3.py
@@ -1,6 +1,12 @@
 # -- coding: utf-8 --
-a = int(input())
-b = int(input())
-for i in range(a,b-1, -1):
-        if i%2!=0:
-            print(i)
+
+def main():
+    a,b = map(int, input().split())
+    if a > b:
+        for i in range(a, b + -1, -1):
+            if i % 2 != 0:
+                print(i)
+    else:
+        print('Число A должно быть больше чем число B')
+            
+main()
