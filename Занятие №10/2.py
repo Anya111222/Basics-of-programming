ec118d1ad4683226bce7e92678527d759888d8af
Anya111222 <146580890+Anya111222@users.noreply.github.com>
2023-12-06 21:21:40 +0300

    Занятие №10

---
+++ Занятие №10/2.py
@@ -0,0 +1,49 @@
+#2
+def openfile():
+    with open('import.txt','r') as file:
+        A = []
+        matrix = file.readlines()
+        for i in matrix:
+            stroke = []
+            for j in i.split():
+                stroke.append(int(j))
+            A.append(stroke)
+        file.close()
+    return A
+
+def mas(A):
+    n = len(A)
+    for i in range(n):
+        for j in range(n):
+            if A[i][j] < 0:
+                A[i][j] = 0
+            else:
+                A[i][j] = 1
+    return A
+
+def mass(A):
+    M = []
+    n = len(A)
+    for i in range(n):
+        stroke = []
+        for j in range(n):
+            if i < j:
+                stroke.append(0)
+                print(0, end=' ')
+            else:
+                stroke.append(A[i][j])
+                print(A[i][j], end=' ')
+        M.append(stroke)
+        print()
+    save(M)
+
+def save(A):
+    with open('save.txt','w') as file:
+        for i in A:
+            out = ' '.join(map(str,i))
+            file.write(out+'\n')
+        file.close()
+            
+
+A = mas(openfile())
+mass(A)
